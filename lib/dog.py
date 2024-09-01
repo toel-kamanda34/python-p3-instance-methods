31fd159268391a73b8b3dc18bf30a30a8474dcf1
toel-kamanda34 <toelokemwa@gmail.com>
2024-09-01 23:29:39 +0300

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,26 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+  def bark(self):
+    print("Woof!")
+  def sit(self):
+    print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
+
+class Person:
+  def talk(self):
+    print("Hello World!")
+
+  def walk(self):
+    print("The person is walking.")  
+
+toel = Person()
+toel.talk()
+toel.walk()
